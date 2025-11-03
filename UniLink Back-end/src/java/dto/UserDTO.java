f9ec7c8fc4900dbb8b385824205ff516733cad30
Vikash Udarsha <udarshav@gmail.com>
2025-11-03 14:43:49 +0530

    Initial commit

---
+++ UniLink Back-end/src/java/dto/UserDTO.java
@@ -0,0 +1,104 @@
+
+package dto;
+
+import entity.Status;
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class UserDTO implements Serializable {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String displayName;
+    private String countryCode;
+    private String contactNo;
+    private String profileImage;
+    private Date createdAt;
+    private Date updatedAt;
+    private Status status;
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+    
+    
+}
